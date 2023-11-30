7c174b66b1cec68e69a875ea20a0354fe71d7cb2
lutusyo <boni>
2023-11-30 11:05:57 +0300

    ch8

--- primeclimatecare_project/settings.py
+++ primeclimatecare_project/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
+
+
 import os
 from pathlib import Path
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fs(_74+6)1tykcxb%-c^knsik#m6&3_yhnnv)t8ys%jj5*71c+'
+#SECRET_KEY = 'django-insecure-fs(_74+6)1tykcxb%-c^knsik#m6&3_yhnnv)t8ys%jj5*71c+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
