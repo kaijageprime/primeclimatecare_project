f10eb0807aa76e48f49684606c92d52af591885d
lutusyo <boni>
2023-12-17 17:07:32 +0300

    ch16

--- primeclimatecare_project/settings.py
+++ primeclimatecare_project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',  
     'allauth', 
     'allauth.account', 
+    'debug_toolbar',
 
 
     # Local
@@ -70,6 +71,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,7 +79,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 
 
 
@@ -87,6 +90,11 @@ MIDDLEWARE = [
     # ... other middleware entries ...
 
 ]
+
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 604800
+CACHE_MIDDLEWARE_KEY_PREFIX = ''
 
 ROOT_URLCONF = 'primeclimatecare_project.urls'
 
@@ -206,3 +214,9 @@ DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 # Stripe
 STRIPE_TEST_PUBLISHABLE_KEY=os.environ.get('STRIPE_TEST_PUBLISHABLE_KEY')
 STRIPE_TEST_SECRET_KEY=os.environ.get('STRIPE_TEST_SECRET_KEY')
+
+
+# django-debug-toolbar
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
--- primeclimatecare_project/urls.py
+++ primeclimatecare_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
+
+
 from django.conf import settings 
 from django.conf.urls.static import static 
+import debug_toolbar
 
 from django.contrib import admin
 from django.urls import path, include
@@ -36,3 +40,10 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
