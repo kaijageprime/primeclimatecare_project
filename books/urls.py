e96540377d93e6208fed4cb8770ad6d8a214fb5f
lutusyo <boni>
2023-12-17 07:07:23 +0300

    ch15

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,14 @@
 import uuid
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchReasultsListView
 
 
 urlpatterns = [
 
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchReasultsListView.as_view(), name='search_results'),
+    
     
 ]
--- books/views.py
+++ books/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 
+from django.db.models import Q
+
 from .models import Book
 
 class BookListView(LoginRequiredMixin, ListView):
@@ -20,3 +22,16 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchReasultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
+
